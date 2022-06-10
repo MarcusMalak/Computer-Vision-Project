5a617f90ec75121ded9753a2e25d88e4e7146a2b
Calin <calin.botoroaga@yahoo.com>
2022-06-10 15:43:26 +0200

    Cleaned the code a bit

--- printfailure/model/model_pretrained_resnet.py
+++ printfailure/model/model_pretrained_resnet.py
@@ -1,19 +1,5 @@
-import torch
-import torch.nn.functional as F
 import torch.nn as nn
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-import torch.backends.cudnn as cudnn
-import numpy as np
 import torchvision
-from torchvision import datasets, models, transforms
-
-
-# Load data: transforms.RandomResizedCrop(224)
-# after ToTensor() : transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 
 
 def get_pretrained_resnet():
@@ -28,13 +14,3 @@ def get_pretrained_resnet():
 
     return model
 
-# model_conv = model_conv.to(device)
-#
-# criterion = nn.CrossEntropyLoss()
-#
-# # Observe that only parameters of final layer are being optimized as
-# # opposed to before.
-# optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
-#
-# # Decay LR by a factor of 0.1 every 7 epochs
-# exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
